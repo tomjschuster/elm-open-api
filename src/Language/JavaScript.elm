3dffe9dbfe7371edc4d921f1d7f671e91f6de767
Tom Schuster <tomjschuster@gmail.com>
2018-09-05 00:23:42 -0400

    exposed ast creation API for module items and declarations

--- src/Language/JavaScript.elm
+++ src/Language/JavaScript.elm
@@ -25,26 +25,308 @@ module Language.JavaScript
         )
 
 import Doc exposing ((|+), Doc)
+import Language.JavaScript.Ast as Ast exposing (..)
 import Regex exposing (Regex)
 import String.Extra
+
+
+test : Ast.Program
+test =
+    module_
+        [ importModule "abc.js"
+        , importAllAs Nothing "tool" "xyz.js"
+        , import_
+            (Just "defaultVar")
+            [ ( "abc", Nothing ), ( "XYZ", Just "xyz" ) ]
+            "123.js"
+        , exportFrom
+            [ ( "abc", Nothing ), ( "XYZ", Just "xyz" ) ]
+            "abc.js"
+        , exportAllFrom "example.js"
+        ]
+
+
+
+{- Missing:
+   - Patterns
+   - Generators, Async Functions, Async Generators
+
+-}
+{- Module -}
+
+
+module_ : List ModuleItem -> Ast.Program
+module_ moduleItems =
+    Module moduleItems
+
+
+importModule : ModulePath -> ModuleItem
+importModule modulePath =
+    ImportItem <| ImportModulePath modulePath
+
+
+import_ :
+    Maybe Identifier
+    -> List ( Identifier, Maybe Identifier )
+    -> ModulePath
+    -> ModuleItem
+import_ defaultId imports modulePath =
+    ImportItem <|
+        Import
+            (NamedImport defaultId (List.map importSpecifier imports))
+            modulePath
+
+
+importAllAs :
+    Maybe Identifier
+    -> Identifier
+    -> ModulePath
+    -> ModuleItem
+importAllAs defaultId importAs modulePath =
+    ImportItem <|
+        Import (NameSpaceImport defaultId importAs) modulePath
+
+
+exportFrom :
+    List ( Identifier, Maybe Identifier )
+    -> ModulePath
+    -> ModuleItem
+exportFrom exports modulePath =
+    ExportItem <|
+        ExportFrom (List.map exportSpecifier exports) modulePath
+
+
+exportAllFrom : ModulePath -> ModuleItem
+exportAllFrom modulePath =
+    ExportItem <| ExportAllFrom modulePath
+
+
+exportConst : List ( Identifier, Maybe Expression ) -> ModuleItem
+exportConst bindings =
+    ExportItem <| DeclarationExport <| LexicalDeclaration <| constDeclaration bindings
+
+
+exportLet : List ( Identifier, Maybe Expression ) -> ModuleItem
+exportLet bindings =
+    ExportItem <| DeclarationExport <| LexicalDeclaration <| letDeclaration bindings
+
+
+exportVar : List ( Identifier, Maybe Expression ) -> ModuleItem
+exportVar bindings =
+    ExportItem <| VariableStatementExport (List.map binding bindings)
+
+
+exportFunction :
+    Identifier
+    -> List ( Identifier, Maybe Expression )
+    -> Maybe Identifier
+    -> List StatementListItem
+    -> ModuleItem
+exportFunction identifier fnParameters rest body =
+    ExportItem <|
+        DeclarationExport <|
+            HoistableDeclaration <|
+                functionDeclaration identifier fnParameters rest body
+
+
+exportClass :
+    Identifier
+    -> Maybe Identifier
+    -> List ( Bool, MethodDefinition )
+    -> ModuleItem
+exportClass identifier extends classElements =
+    ExportItem <| DeclarationExport <| classDeclaration identifier extends classElements
+
+
+exportDefault : Expression -> ModuleItem
+exportDefault expression =
+    ExportItem <| DefaultExpressionExport expression
+
+
+exportDefaultFunction :
+    Identifier
+    -> List ( Identifier, Maybe Expression )
+    -> Maybe Identifier
+    -> List StatementListItem
+    -> ModuleItem
+exportDefaultFunction identifier fnParameters rest body =
+    ExportItem <|
+        DefaultHoistableDeclarationExport <|
+            functionDeclaration identifier fnParameters rest body
+
+
+exportDefaultClass :
+    Identifier
+    -> Maybe Identifier
+    -> List ( Bool, MethodDefinition )
+    -> ModuleItem
+exportDefaultClass identifier extends classElements =
+    ExportItem <|
+        DefaultClassDeclarationExport identifier
+            (heritage extends)
+            (List.map classElement classElements)
+
+
+
+-- Module Helpers
+
+
+importSpecifier :
+    ( Identifier, Maybe Identifier )
+    -> ImportSpecifier
+importSpecifier ( identifier, alias ) =
+    alias
+        |> Maybe.map (AsImportSpecifier identifier)
+        |> Maybe.withDefault (ImportSpecifier identifier)
+
+
+exportSpecifier : ( Identifier, Maybe Identifier ) -> ExportSpecifier
+exportSpecifier ( identifier, alias ) =
+    alias
+        |> Maybe.map (AsExportSpecifier identifier)
+        |> Maybe.withDefault (ExportSpecifier identifier)
+
+
+
+{- Statements -}
+
+
+const : List ( Identifier, Maybe Expression ) -> StatementListItem
+const bindings =
+    DeclarationItem <| LexicalDeclaration <| constDeclaration bindings
+
+
+let_ : List ( Identifier, Maybe Expression ) -> StatementListItem
+let_ bindings =
+    DeclarationItem <| LexicalDeclaration <| letDeclaration bindings
+
+
+var : List ( Identifier, Maybe Expression ) -> StatementListItem
+var bindings =
+    StatementItem <| VariableStatement (List.map binding bindings)
+
+
+function :
+    Identifier
+    -> List ( Identifier, Maybe Expression )
+    -> Maybe Identifier
+    -> List StatementListItem
+    -> StatementListItem
+function identifier fnParameters rest statements =
+    DeclarationItem <|
+        HoistableDeclaration <|
+            functionDeclaration identifier fnParameters rest statements
+
+
+class :
+    Identifier
+    -> Maybe Identifier
+    -> List ( Bool, MethodDefinition )
+    -> StatementListItem
+class identifier extends classElements =
+    DeclarationItem <|
+        ClassDeclaration identifier
+            (heritage extends)
+            (List.map classElement classElements)
+
+
+
+-- Declaration Helper
+{- Declarations -}
+
+
+constDeclaration : List ( Identifier, Maybe Expression ) -> LexicalDeclaration
+constDeclaration bindings =
+    Const (List.map binding bindings)
+
+
+letDeclaration : List ( Identifier, Maybe Expression ) -> LexicalDeclaration
+letDeclaration bindings =
+    Let (List.map binding bindings)
+
+
+functionDeclaration :
+    Identifier
+    -> List ( Identifier, Maybe Expression )
+    -> Maybe Identifier
+    -> List StatementListItem
+    -> HoistableDeclaration
+functionDeclaration identifier fnParameters rest statements =
+    FunctionDeclaration identifier
+        (parameters fnParameters rest)
+        (Block statements)
+
+
+classDeclaration :
+    Identifier
+    -> Maybe Identifier
+    -> List ( Bool, MethodDefinition )
+    -> Declaration
+classDeclaration identifier extends classElements =
+    ClassDeclaration identifier
+        (heritage extends)
+        (List.map classElement classElements)
+
+
+
+-- Declaration Helpers
+
+
+binding : ( Identifier, Maybe Expression ) -> Binding
+binding ( identifier, initializer ) =
+    IdentifierBinding identifier initializer
+
+
+parameters :
+    List ( Identifier, Maybe Expression )
+    -> Maybe Identifier
+    -> Parameters
+parameters fnParameters rest =
+    Parameters
+        (List.map parameter fnParameters)
+        (Maybe.map BindingRestElementIdentifier rest)
+
+
+parameter : ( Identifier, Maybe Expression ) -> BindingElement
+parameter ( identifier, initializer ) =
+    SingleNameBindingElement identifier initializer
+
+
+heritage : Maybe Identifier -> Heritage
+heritage extends =
+    case extends of
+        Just identifier ->
+            Heritage identifier
+
+        nothing ->
+            NoHeritage
+
+
+classElement : ( Bool, MethodDefinition ) -> ClassElement
+classElement ( static, methodDefinition ) =
+    if static then
+        StaticClassMethod methodDefinition
+    else
+        ClassMethod methodDefinition
+
 
 
 {- Statements -}
 -- Declarations
 
 
-var : String -> Doc -> Doc
-var variable expression =
+varOBS : String -> Doc -> Doc
+varOBS variable expression =
     declare "var" variable expression
 
 
-let_ : String -> Doc -> Doc
-let_ variable expression =
+let_OBS : String -> Doc -> Doc
+let_OBS variable expression =
     declare "let" variable expression
 
 
-const : String -> Doc -> Doc
-const variable expression =
+constOBS : String -> Doc -> Doc
+constOBS variable expression =
     declare "const" variable expression
 
 
@@ -82,8 +364,8 @@ declare label variable expression =
 -- Functions
 
 
-function : List String -> List Doc -> Doc
-function argList statements =
+functionOBS : List String -> List Doc -> Doc
+functionOBS argList statements =
     Doc.string "function"
         |+ declareArgs argList
         |+ block statements
